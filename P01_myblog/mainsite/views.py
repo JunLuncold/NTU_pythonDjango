935570d0b22dec0fb4b69f7996fa68c0b6f9c3ca
JunLuncold <130819955+JunLuncold@users.noreply.github.com>
2024-07-24 12:46:32 +0800

    - 更新最新進度
    - 更新最新進度

--- P01_myblog/mainsite/views.py
+++ P01_myblog/mainsite/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.template.loader import get_template 
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from datetime import datetime
 from .models import Post
+from rest_framework import generics
+from .serializers import UserSerializer
 # Create your views here.
 
-def homepage(request):
-    posts = Post.objects.all()
-    post_list = list()
-    for count, post in enumerate(posts):
-        post_list.append("No.{}:".format(str(count)) + str(post)+ str(post.pub_date) + "<br>")
-        post_list.append("<small>" + str(post.body) + "</small><br></br>")
-    return HttpResponse(post_list)
+def homepage(request):     
+    template = get_template('index.html') #獲取網頁要貼文的網頁
+    posts = Post.objects.all() #獲取 models.py底下的資料
+    now = datetime.now() #現在時間
+    html = template.render(locals()) #渲染成 html 
+    return HttpResponse(html)
+
+def showpost(request, slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+    except:
+        return redirect('/')
+
+class UserList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = UserSerializer
--- P01_myblog/myblog/urls.py
+++ P01_myblog/myblog/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from mainsite.views import homepage
+from django.urls import path,include
+from mainsite.views import homepage,showpost
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage),
+    path('post/<slug:slug>/',showpost),
+    #path('api/',include('mainsite.urls')),
 ]
